4132008ac4527c6077cb90aff2be6d80be4492ab
lkhlkh23 <lkhlkh09@gmail.com>
2018-10-01 16:51:20 +0900

    Modify Step 5-1

--- src/main/java/ladder/domain/LadderLine.java
+++ src/main/java/ladder/domain/LadderLine.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class LadderLine {
 	private int countOfPerson;
 	private List<Boolean> points;
-	private List<Pointer> pointers;
 
 	public LadderLine(int countOfPerson) {
 		this.countOfPerson = countOfPerson;
@@ -14,20 +13,12 @@ public class LadderLine {
 	public LadderLine(int countOfPerson, List<Participation> participations) {
 		this(countOfPerson);
 		this.points = new ArrayList<>();
-		initPointers(participations);
 	}
 
 	/* 테스트를 위한 생성자! */
 	public LadderLine(int countOfPerson, List<Participation> participations, List<Boolean> points) {
 		this(countOfPerson);
 		this.points = points;
-	}
-
-	private void initPointers(List<Participation> participations) {
-		pointers = new ArrayList<>();
-		for(Participation participation : participations) {
-			pointers.add(participation.getPointer());
-		}
 	}
 	
 	public void addPoint(boolean isBar) {
@@ -69,11 +60,12 @@ public class LadderLine {
 		return Collections.unmodifiableList(points);
 	}
 
-	public void moveDistance(int index) {
-		if(pointers.get(index).isMoveLeft(points)) {
-			pointers.get(index).moveX(-1);
-		} else if(pointers.get(index).isMoveRight(points)) {
-			pointers.get(index).moveX(1);
+	public void moveDistance(Participation participation) {
+		if(participation.getPointer().isMoveLeft(points)) {
+			participation.getPointer().moveX(-1);
+		} else if(participation.getPointer().isMoveRight(points)) {
+			participation.getPointer().moveX(1);
 		}
 	}
+
 }
